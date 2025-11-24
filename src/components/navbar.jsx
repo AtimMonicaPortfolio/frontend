5c9f544562e5a4ffebc158494e84adc1bed7c8fb
ATIM-MONICA <monicadeann.ja@gmail.com>
2025-11-24 14:22:06 +0300

    add links to pages-on mobile menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -30,6 +30,20 @@ export default function Navbar() {
         ☰
       </button>
 
+      {/* MOBILE MENU */}
+      <div
+        className={`md:hidden flex flex-col gap-4 mt-4 transition-all duration-300 ${
+          open ? "max-h-96 opacity-100" : "max-h-0 opacity-0 overflow-hidden"
+        }`}
+      >
+        <a href="/" className="px-4 py-2">Home</a>
+        <a href="/about" className="px-4 py-2">About</a>
+        <a href="/skills" className="px-4 py-2">Skills</a>
+        <a href="/projects" className="px-4 py-2">Projects</a>
+        <a href="/contact" className="px-4 py-2">Contact</a>
+      </div>
+
+
       {/* NAV LINKS */}
       <div
         className={`
