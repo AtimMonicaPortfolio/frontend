e797d425bf269f771187b29b1aa4924a95612482
ATIM-MONICA <monicadeann.ja@gmail.com>
2025-11-25 14:33:47 +0300

    load resume PDF from assets and add hover animation to download button

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import Footer from "../components/footer";
 import { Link } from "react-router-dom";
+import Resume from "../assets/atim_monica_resume.pdf";
 
 
 export default function Home() {
@@ -52,29 +53,33 @@ export default function Home() {
           </Link>
 
           <a
-            href="/Atim Monica CV01.pdf"
-            download
-            className="flex items-center justify-center gap-2  
-            bg-tealBlue text-white px-6 py-3 rounded-lg shadow hover:bg-deepBlue transition
-            w-full sm:w-auto                                    
-            "
+          href={Resume}
+          download="Atim-Monica-Resume.pdf"
+          className="group flex items-center justify-center gap-2  
+            bg-tealBlue text-white px-6 py-3 rounded-lg shadow 
+            hover:bg-deepBlue transition w-full sm:w-auto 
+            transform hover:scale-105 duration-300"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 transform transition-all duration-300 group-hover:-translate-y-1"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-              />
-            </svg>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+            />
+          </svg>
+
+          <span className="transform transition-all duration-300 group-hover:-translate-y-1">
             Download Resume
-          </a>
+          </span>
+        </a>
+
         </div>
       </main>
 
